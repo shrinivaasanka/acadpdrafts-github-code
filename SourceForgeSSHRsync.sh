b6a177cb87e44290396bc713fb3eb3361208a3ec
KaShrinivaasan <kashrinivaasan@kashrinivaasan-Inspiron-1545.(none)>
2013-08-24 14:36:42 +0530

    Updated RSync script with # prepended in GNU copyright license

--- SourceForgeSSHRsync.sh
+++ SourceForgeSSHRsync.sh
@@ -1,29 +1,30 @@
-acadpdrafts - My academic publication drafts and related documents including ID Proofs
-
-Copyright: Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
---------------------------------------------------------------------------------------------------
-Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
-Independent Open Source Developer, Researcher and Consultant
-Ph: 9003082186, 9791165980
-Open Source Products Profile(Krishna iResearch): http://sourceforge.net/users/ka_shrinivaasan
-Personal website(research): https://sites.google.com/site/kuja27/
-emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com, kashrinivaasan@live.com
---------------------------------------------------------------------------------------------------
-*****************************************************************************************/
+#*****************************************************************************************/
+#acadpdrafts - My academic publication drafts and related documents including ID Proofs
+#
+#Copyright: Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#--------------------------------------------------------------------------------------------------
+#Srinivasan Kannan (alias) Ka.Shrinivaasan (alias) Shrinivas Kannan
+#Independent Open Source Developer, Researcher and Consultant
+#Ph: 9003082186, 9791165980
+#Open Source Products Profile(Krishna iResearch): http://sourceforge.net/users/ka_shrinivaasan
+#Personal website(research): https://sites.google.com/site/kuja27/
+#emails: ka.shrinivaasan@gmail.com, shrinivas.kannan@gmail.com, kashrinivaasan@live.com
+#--------------------------------------------------------------------------------------------------
+#*****************************************************************************************/
 
 #!/bin/bash
 rsync -avz --progress -e ssh $1 ka_shrinivaasan@frs.sourceforge.net:/home/frs/project/acadpdrafts/
