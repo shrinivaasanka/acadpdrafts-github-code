3e73edc6b930a91a99a324943ac7ce17cbff9a90
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2014-09-26 13:33:11 +0530

    Added --partial flag (-P) to rsync commandline for download resume

--- SourceForgeSSHRsync.sh
+++ SourceForgeSSHRsync.sh
@@ -27,4 +27,4 @@
 #*****************************************************************************************/
 
 #!/bin/bash
-rsync -avz --progress -e ssh $1 ka_shrinivaasan@frs.sourceforge.net:/home/frs/project/acadpdrafts/
+rsync -avzP --progress -e ssh $1 ka_shrinivaasan@frs.sourceforge.net:/home/frs/project/acadpdrafts/
